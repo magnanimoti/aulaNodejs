b26aeef1d22e49bf1c03a294fab68619345510a0
Josimar Pereira de Lima <magnanimo.pl@gmail.com.br>
2016-10-24 14:35:16 -0200

    criação do modelo usuario

---
+++ app/controller/home_controller.js
@@ -0,0 +1,14 @@
+var Usuario = require('../models/usuario');	
+
+var HomeController = {
+	index: function(request, response, next) {
+ 	  response.render('index', { title: 'Express' });
+	},
+	usuario: function(request, response, next) {
+	  var usuario = new Usuario();
+	  console.log(usuario);
+ 	  response.send('Olá usuario');
+	}
+}
+
+module.exports = HomeController;
