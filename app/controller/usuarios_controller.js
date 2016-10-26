781f5b904002eedcd20e209b84713fe8a7f27fbd
Josimar Pereira de Lima <magnanimo.pl@gmail.com.br>
2016-10-26 15:44:40 -0200

    atualização de usuario usando PUT

--- app/controller/usuarios_controller.js
+++ app/controller/usuarios_controller.js
@@ -52,6 +52,32 @@ var UsuariosController = {
 				response.status(201).send({mensagem: 'Usuario criado com sucesso'});
 			}
 		});
+	},
+	atualizar: function(request,response, next){
+		Usuario.buscarPorId(request.body.id, function(retorno){
+			if(retorno.usuario.id === undefined){
+				response.status(400).send({
+					error: 'erro ao atualizar, id de usuario não encontrado'
+				});
+			}else{
+				var usuario = new Usuario();
+				usuario.id = request.body.id;
+				usuario.nome=request.body.nome;
+				usuario.email=request.body.email;
+				usuario.senha=request.body.senha;
+				usuario.login=request.body.login;
+				usuario.salvar(function(retorno){
+					if(retorno.error){
+						response.status(500).send({
+							error: 'erro ao atualizar usuario  (' + retorno.message + ')'
+						});
+					}else{
+						response.status(200).send({mensagem: 'Usuario atualizado com sucesso'});
+					}
+				});		
+			}
+		});
+		
 	}
 };
 
--- spec/app/controller/usuarios_controller_spec.js
+++ spec/app/controller/usuarios_controller_spec.js
@@ -67,7 +67,7 @@ describe("Controle de usuarios", function(){
 		});	
 	});
 	describe(" POST /usuarios.json criar [usuario]", function(){
-		it("returns status code 200 e [usuario] criado",function(done){
+		it("returns status code 201 e  mensagem de [usuario] criado",function(done){
 			request.post({url:host +"/usuarios.json",form: {nome:'user2', email: 'user2@user2', senha: '***', login: 'user2'}}, function(error, response, body){
 				if(response === undefined){
 					console.log("Não consegui localizar o servidor");
@@ -85,4 +85,55 @@ describe("Controle de usuarios", function(){
 		});	
 	});
 
-});
+	describe(" PUT /usuarios.json atualizar [usuario]", function(){
+		var usuarioCadastrado;
+		beforeEach(function(done){ //executa todo este codigo antes de fazer  it
+			Usuario.excluirTodos(function(retorno1){
+				var usuario = new Usuario();
+				usuario.nome = "user3";
+				usuario.login = "user3";
+				usuario.senha = "***";
+				usuario.email = "user3@user3";
+				usuario.salvar(function(retorno2){
+					Usuario.todos(function(retorno3){
+						if(!retorno3.error){
+							usuarioCadastrado = retorno3.usuarios[0];	
+						}
+						done();
+					});
+				});
+			});
+		});
+		it("returns status code 200 e [usuario] atualizado",function(done){	
+			usuarioCadastrado.nome = "user";
+			request.put({url:host +"/usuarios.json",form: usuarioCadastrado}, function(error, response, body){
+				if(response === undefined){
+					console.log("Não consegui localizar o servidor");
+					expect(503).toBe(200);
+				}else{
+					expect(response.statusCode).toBe(200);
+					var json = JSON.parse(response.body);
+					//console.log(json); 	
+					expect(json.mensagem).toBe("Usuario atualizado com sucesso");
+					Usuario.buscarPorId(usuarioCadastrado.id, function(retorno){
+						expect(retorno.usuario.nome).toBe("user");
+						done();
+					});
+				}	
+				done();
+			});
+		});
+		it("returns status code 200 e [usuario] criado",function(done){	
+			request.put({url:host +"/usuarios.json",form: {}}, function(error, response, body){
+				if(response === undefined){
+					console.log("Não consegui localizar o servidor");
+					expect(503).toBe(200);
+				}else{
+					expect(response.statusCode).toBe(400);
+					done();
+				}	
+			});
+		});
+	});
+
+});	
